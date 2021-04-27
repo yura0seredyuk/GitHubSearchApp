1dcafec863045fce10fe582f0e8f7bc03c8a8c9f
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-04-27 23:31:18 +0300

    render list of repos

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Right,
+} from 'native-base';
 
 const App = () => {
   const BASE_URL = 'https://api.github.com/search/repositories?q=yura0seredyuk';
 
   const [name, setName] = useState('');
   const [avatarUrl, setAvatarUrl] = useState('');
+  const [reposUrl, setReposUrl] = useState('');
+  const [reposList, setReposList] = useState('');
 
   useEffect(() => {
     fetch(BASE_URL)
@@ -13,19 +28,54 @@ const App = () => {
       .then(data => {
         setName(data.items[0].name);
         setAvatarUrl(data.items[0].owner.avatar_url);
+        setReposUrl(data.items[0].owner.repos_url);
+      });
+
+    fetch(reposUrl)
+      .then(response => response.json())
+      .then(data => {
+        setReposList(data);
       });
   }, []);
 
+  function displayCard() {
+    return reposList.map(repos => {
+      return (
+        <Card>
+          <CardItem>
+            <Left>
+              <Thumbnail source={{uri: `${avatarUrl}`}} />
+              <Body>
+                <Text>{repos.name}</Text>
+                <Text note>{name}</Text>
+              </Body>
+            </Left>
+          </CardItem>
+          <CardItem>
+            <Left>
+              <Button transparent>
+                <Text>12 Likes</Text>
+              </Button>
+            </Left>
+            <Body>
+              <Button transparent>
+                <Text>4 Comments</Text>
+              </Button>
+            </Body>
+            <Right>
+              <Text>11h ago</Text>
+            </Right>
+          </CardItem>
+        </Card>
+      );
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{name}</Text>
-      <Image
-        style={styles.image}
-        source={{
-          uri: `${avatarUrl}`,
-        }}
-      />
-    </View>
+    <Container>
+      <Header />
+      <Content>{displayCard()}</Content>
+    </Container>
   );
 };
 
