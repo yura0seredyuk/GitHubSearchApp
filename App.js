2eaeb339aa2b142e570c4d5fca77a87b8a73c5c2
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-04-27 22:16:08 +0300

    fetch data from api

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 const App = () => {
+  const BASE_URL = 'https://api.github.com/search/repositories?q=yura0seredyuk';
+
+  const [name, setName] = useState('');
+  const [avatarUrl, setAvatarUrl] = useState('');
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(response => response.json())
+      .then(data => {
+        setName(data.items[0].name);
+        setAvatarUrl(data.items[0].owner.avatar_url);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Test</Text>
+      <Text style={styles.text}>{name}</Text>
+      <Image
+        style={styles.image}
+        source={{
+          uri: `${avatarUrl}`,
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
+    flex: 1,
+    backgroundColor: 'white',
   },
   text: {
+    margin: 30,
     color: 'red',
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
 
